3a242f49e119c35f89438d8f0d553626ff5e4db6
lisalisabobisa <lisamabeles@gmail.com>
2021-02-15 14:22:09 -0800

    first

---
+++ js/script.js
@@ -0,0 +1,136 @@
+// Step 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+
+// Step 2
+// 3
+// "Hello"
+// false
+// null
+
+// Step 3
+// let anExpression = 5 * 3 / 4;
+// let num = [4,6,2,1];
+
+// Step 4
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zip;
+// let code;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+
+// Step 5
+// let city;
+// let state;
+// city = "San Diego";
+// state = "California";
+// let lastName = "Abeles";
+// let city = "San Diego", state="California";
+
+
+// Step 6
+// let beers = 99 + " beers on the wall";
+// console.log(beers);
+
+// Step 7
+// let thisStuff = true + " This stuff is hard";
+// console.log(thisStuff);
+
+// let var1 = 1 + false;
+// console.log(var1);
+
+// Step 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+// correction to I\'m
+
+
+// Step 9
+// let y = null;
+// alert(y);
+// let x;
+// alert(x);
+
+// Step 10
+// let num = "Hello";
+// let num = Hello;
+// let num = 42;
+// let num = true;
+// let num = {x:1, y:2};
+// console.log(typeof num);
+
+// Step 11
+// alert("Hello " + "Lisa" + ", welcome to the JavaScript class!");
+
+// Step 12
+// let name = "Lisa";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// Step 13
+// let name = "Lisa";
+// let course = "JavaScript";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// Step 14
+// let name = "Lisa";
+// let course = "JavaScript";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// Step 15
+// let name = prompt("What is your name?");
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// Step 16
+// let course = prompt("What class are you taking?");
+// alert("Hello Lisa, welcome to " + course + "!");
+
+// Step 17
+// let x = 10, y = 20;
+// console.log(x + y);
+
+// Step 18
+// let x = 20;
+// console.log(x + 20);
+
+// Step 19
+// let x = 20;
+// console.log(x * 5);
+
+// Step 20
+// let x = 20 % 3;
+// console.log(x / 1);
+
+// Step 21
+// let a = 1;
+// let b = 2;
+// console.log(a >= 1 || b <= 2);
+
+// Step 22
+// let a = 3;
+// let b = -2;
+// console.log(a > 0 && b>0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
